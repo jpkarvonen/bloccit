524a6bec130cbdb992e908e244c320e0fc3214f2
EC2 Default User <ec2-user@ip-172-31-10-250.us-east-2.compute.internal>
2018-02-22 17:02:37 +0000

    Assignment 16 Bloccit: complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@ require 'random_data'
     )
 end
 
+unique = Post.find_or_create_by!(title: "Hello", body: "I'm new here.")
+
 posts = Post.all
 
 100.times do 
@@ -15,6 +17,8 @@ posts = Post.all
         body: RandomData.random_paragraph
     )
 end
+
+Comment.find_or_create_by!(post: unique, body: "Welcome!")
 
 puts "Seeds finished"
 puts "#{Post.count} posts created"
