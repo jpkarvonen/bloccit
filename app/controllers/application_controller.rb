bcff4ad6f48c4619c0965870a2a368020563c415
EC2 Default User <ec2-user@ip-172-31-10-250.us-east-2.compute.internal>
2018-03-02 21:46:44 +0000

    Assignment 24 Bloccit: complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  #sometimes would get 'ActionController::InvalidAuthenticityToken (ActionController::InvalidAuthenticityToken)' so i added this line to circumvent the issues. worked on this with my mentor and we couldn't
+  #determine why this was happening so i ended up doing this
+  skip_before_action :verify_authenticity_token
 end
