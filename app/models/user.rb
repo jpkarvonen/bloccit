9b8a4e60c9e7503e284cf301703d4cfb1f67c9f6
EC2 Default User <ec2-user@ip-172-31-10-250.us-east-2.compute.internal>
2018-03-02 18:06:27 +0000

    Assignment 23 Bloccit: complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
     before_save { self.email = email.downcase if email.present? }
+    before_save {
+        if name
+            result = []
+            self.name.split.each do |n|
+                result << n.capitalize
+            end
+            self.name = result.join(" ")
+        end
+    }
 
     validates :name, length: { minimum: 1, maximum: 100 }, presence: true
     validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
