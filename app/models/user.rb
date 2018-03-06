2c314722c84e3e179c8bb0e1f12e2f247f63c664
EC2 Default User <ec2-user@ip-172-31-10-250.us-east-2.compute.internal>
2018-03-06 20:23:22 +0000

    Checkpoint 27 Bloccit: complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-    
+    has_many :posts, dependent: :destroy
     
     before_save { self.email = email.downcase if email.present? }
-    has_many :posts, dependent: :destroy
+    before_save { self.role ||= :member }
+   
 
     validates :name, length: { minimum: 1, maximum: 100 }, presence: true
     validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -12,4 +13,8 @@ class User < ApplicationRecord
              uniqueness: { case_sensitive: false },
              length: { minimum: 3, maximum: 254 }
     has_secure_password
+    
+    
+    enum role: [:member, :admin]
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180305174112) do
+ActiveRecord::Schema.define(version: 20180306183206) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20180305174112) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
