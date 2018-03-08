7283b81fe4c165c63d6d68df08dc6745c6ae48a9
EC2 Default User <ec2-user@ip-172-31-10-250.us-east-2.compute.internal>
2018-03-08 01:52:49 +0000

    fix attemptx

--- db/migrate/20180302171041_create_users.rb
+++ db/migrate/20180302171041_create_users.rb
@@ -1,10 +1,5 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
-    
-    # when running rails db:migrate console logged: 'SQLite3::SQLException: table "users" already exists'
-    # i researched the issue and implemented 'drop_table :users'. this seems to have resolved the issue. 
-    # id appreciate feedback on this and if this fix may cause issues
-    drop_table :users
 
     create_table :users do |t|
       t.string :name
