456130717b335858499a511895f77f47f5cd860b
EC2 Default User <ec2-user@ip-172-31-10-250.us-east-2.compute.internal>
2018-03-13 15:27:46 +0000

    Assignment 31 Bloccit: revised

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,18 @@
 module UsersHelper
-    def user_has_no_posts?
-        @user.posts.empty?
+    def user_has_posts(user)
+        if user.posts.empty?
+            "<p>OMG there's nothing here! Get on it, #{user.name}!</p>".html_safe
+        else
+            render user.posts
+        end
     end
+
     
-    def user_has_no_comments?
-        @user.comments.empty?
+    def user_has_comments(user)
+        if user.comments.empty?
+            "<p>Do you even know how to comment, #{user.name}???</p>".html_safe
+        else
+            render user.comments
+        end
     end
 end
